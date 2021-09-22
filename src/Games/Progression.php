8cac12dbb40b609b283955c5f61f37c081e965b3
Grigory Tydykov <tydykov2022@mail.ru>
2021-09-22 09:47:16 +0600

    add game brain-progression

---
+++ src/Games/Progression.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Brain\Games\Games\Progression;
+
+use CONST Brain\Games\Engine\ATTEMPTS;
+
+const MESSAGE = 'What number is missing in the progression?';
+const PROGRESSION_LENGTH = 10;
+
+function getProgression($start, $step, $length): array
+{
+    $progression = [];
+    for ($i = $start; $i < $start + $length * $step; $i += $step) {
+        $progression[] = $i;
+    }
+    return $progression;
+}
+
+function start()
+{
+    $data = [];
+    for ($i = 1; $i <= ATTEMPTS; $i++) {
+        $start = mt_rand(1, 100);
+        $step = mt_rand(1, 10);
+        $positionOfTheMissingNumber = mt_rand(0, PROGRESSION_LENGTH - 1);
+        $progression = getProgression($start, $step, PROGRESSION_LENGTH);
+        $missingNumber = $progression[$positionOfTheMissingNumber];
+        $progression[$positionOfTheMissingNumber] = '..';
+        $data[$i][] = implode(' ', $progression);
+        $data[$i][] = (string) $missingNumber;
+    }
+    \Brain\Games\Engine\run($data, MESSAGE);
+}
